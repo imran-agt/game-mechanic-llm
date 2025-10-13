57100c4e45bf0e8601ac1b2b3ca9d9a1c2410fb0
iarafat <iarafat@agtslots.com>
2025-10-13 11:01:04 -0700

    Updated the logo and login screen to fit with the customized settings

--- server/utils/boot/MetaGenerator.js
+++ server/utils/boot/MetaGenerator.js
@@ -45,21 +45,21 @@ class MetaGenerator {
       {
         tag: "title",
         props: null,
-        content: "AnythingLLM | Your personal LLM trained on anything",
+        content: "Game Mechanic | AGT Coder LLM",
       },
 
       {
         tag: "meta",
         props: {
           name: "title",
-          content: "AnythingLLM | Your personal LLM trained on anything",
+          content: "Game Mechanic | AGT Coder LLM",
         },
       },
       {
         tag: "meta",
         props: {
           description: "title",
-          content: "AnythingLLM | Your personal LLM trained on anything",
+          content: "Game Mechanic | AGT Coder LLM",
         },
       },
 
@@ -73,14 +73,14 @@ class MetaGenerator {
         tag: "meta",
         props: {
           property: "og:title",
-          content: "AnythingLLM | Your personal LLM trained on anything",
+          content: "Game Mechanic | AGT Coder LLM",
         },
       },
       {
         tag: "meta",
         props: {
           property: "og:description",
-          content: "AnythingLLM | Your personal LLM trained on anything",
+          content: "Game Mechanic | AGT Coder LLM",
         },
       },
       {
@@ -105,14 +105,14 @@ class MetaGenerator {
         tag: "meta",
         props: {
           property: "twitter:title",
-          content: "AnythingLLM | Your personal LLM trained on anything",
+          content: "Game Mechanic | AGT Coder LLM",
         },
       },
       {
         tag: "meta",
         props: {
           property: "twitter:description",
-          content: "AnythingLLM | Your personal LLM trained on anything",
+          content: "Game Mechanic | AGT Coder LLM",
         },
       },
       {
@@ -191,7 +191,7 @@ class MetaGenerator {
           props: null,
           content:
             customTitle ??
-            "AnythingLLM | Your personal LLM trained on anything",
+            "Game Mechanic | AGT Coder LLM",
         },
       ];
     }
